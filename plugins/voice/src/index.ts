53b3334137bf7c94b0609c1a1b1645f2df8a2cb3
RONAK-A1647 <codeitronak226277@gmail.com>
2026-08-03 17:32:24 +0000

    Expose the newly created dependency and VAD APIs from the voice plugin

--- plugins/voice/src/index.ts
+++ plugins/voice/src/index.ts
@@ -3,3 +3,14 @@ export { playAudio } from './speaker.js';
 export { transcribeAudio } from './stt.js';
 export { synthesizeSpeech } from './tts.js';
 export { playPhrase } from './play-phrase.js';
+export {
+	checkDependenciesInstalled,
+	installDependencies,
+	type DependencyCheckResult,
+	type InstallDependenciesOptions,
+} from './dependencies.js';
+export {
+	VadEngine,
+	createVadEngine,
+	type VadEngineOptions,
+} from './vad.js';
