96f075411f44aa32b8d333354b330d0e0406bc0a
Tushar <tusharanshu18@gmail.com>
2026-09-03 18:39:06 +0530

    feat(text-input): add Home/End key handling for cursor navigation

--- source/components/text-input.tsx
+++ source/components/text-input.tsx
@@ -167,7 +167,11 @@ function TextInput({
 			let nextValue = originalValueRef.current;
 			let nextCursorWidth = 0;
 
-			if (key.ctrl) {
+			if (key.home) {
+				nextCursorOffset = 0;
+			} else if (key.end) {
+				nextCursorOffset = originalValueRef.current.length;
+			} else if (key.ctrl) {
 				if (key.leftArrow) {
 					// Ctrl+Left: jump to start of previous word
 					if (showCursor) {
